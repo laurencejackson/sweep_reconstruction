e4dc2b8acac9ae4cea1f54448c30c7018f0496e4
Laurence Jackson <Laurence.Jackson@kcl.ac.uk>
2019-06-14 14:51:12 +0100

    started work on respiration estimation module

--- sweeprecon/applications/app_sort_image_data.py
+++ sweeprecon/applications/app_sort_image_data.py
@@ -13,17 +13,19 @@ from sweeprecon.io.ImageData import ImageData
 
 def app_sort_image_data(args=None):
     """
-    Sort image data
+    Sorts image data from 4D slice+dynamics to 3D with by interleaving the dynamics with the slice positions
+    :param args: arguments parsed through command line ( run with [-h] to see required parameters)
     :return:
     """
+
     print('\n________________________ Sorting image data ________________________\n')
 
     # Check if function if being run as part of pipeline or by itself
-    if not args:
+    if args is None:
 
         # parse arguments
-        input_vars = ArgParser(description="Sorts image data such that the temporal dimension is interleaved "
-                                           "with the slice direction creating a 3D output")
+        input_vars = ArgParser(description="Sorts image data from 4D slice+dynamics to 3D with by interleaving the "
+                                           "dynamics with the slice positions")
 
         # required
         input_vars.add_input_file(required=True)
@@ -42,8 +44,9 @@ def app_sort_image_data(args=None):
 
     # save output
     basename = os.path.basename(args.input)
-    image.write_nii(basename, prefix='IMG_3D_')
+    image.write_nii(basename, prefix='IMG_3D_', dirpath='./test_dump')
 
-    print('  Sorting data complete')
+    # Done
+    print('Sorting data complete')
 
     return
--- sweeprecon/io/ImageData.py
+++ sweeprecon/io/ImageData.py
@@ -16,23 +16,24 @@ class ImageData(object):
         Initialise ImageData object
         :param file_path: path to NIfTI image to work with
         """
+
         # Works only for NIfTI images for now
         if not file_path.endswith('.nii.gz') or file_path.endswith('.nii'):
             raise IOError('Invalid input file, must be in NIfTI format (.nii or .nii.gz)')
 
         self._nii = nib.load(file_path)
-        self._img = self.get_data()
-
-    def set_data(self, img):
-        self._img = img
+        self._img = self._nii.get_fdata()  # default _img data to the existing data object
 
     def get_data(self):
         """Returns image data as numpy array"""
-        return self._nii.get_fdata()
+        return self._img
 
     def get_hdr(self):
         """Returns header information"""
         return self._nii.header
+
+    def set_data(self, img):
+        self._img = img
 
     def sort_4d_to_3d(self):
         """Sorts image data from 4D to 3D and writes new NIfTI"""
@@ -46,7 +47,7 @@ class ImageData(object):
         # reshape data
         img_reshape = np.reshape(self._img, (self._img.shape[0], self._img.shape[1], self._img.shape[2] * self._img.shape[3]))
 
-        # modify header to preserve NIfTI geometry
+        # modify header meta keys to preserve NIfTI geometry
         self._nii.affine[:, 2] = self._nii.affine[:, 2] / n_dynamics
         self._nii.header['srow_x'][2] = self._nii.header['srow_x'][2] / n_dynamics
         self._nii.header['srow_y'][2] = self._nii.header['srow_y'][2] / n_dynamics
@@ -56,11 +57,14 @@ class ImageData(object):
         self._nii.header['dim'][4] = 1
         self._nii.header['xyzt_units'] = 2
         self._nii.header['pixdim'][3] = self._nii.header['pixdim'][3] / n_dynamics
-        self._nii.header['pixdim'][4] = 0
 
+        # time/slice used later to calculate sampling frequency
+        self._nii.header['pixdim'][4] = self._nii.header['pixdim'][4] / self._img.shape[3]
+
+        print('Converted data size from {0} to {1}'.format(self._img.shape, img_reshape.shape))
         self.set_data(img_reshape)
 
-    def write_nii(self, filename, dirpath=None, prefix=''):
+    def write_nii(self, filename, dirpath=None, prefix=None):
         """
         Saves NIfTI image in working directory
         :param dirpath: directory to save NIfTI file to
