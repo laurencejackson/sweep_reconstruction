91c9f7ad8326775565bbd3f68d19a891a5cd0b9f
Laurence Jackson <Laurence.Jackson@kcl.ac.uk>
2019-07-05 17:14:39 +0100

    correction to threading

--- sweeprecon/ResampleData.py
+++ sweeprecon/ResampleData.py
@@ -5,6 +5,12 @@ Laurence Jackson, BME, KCL 2019
 """
 
 import os
+# limit threading to reduce cpu overhead in parallel processes
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ['OPENBLAS_NUM_THREADS'] = '1'
+
 import sys
 import copy
 import time
@@ -52,7 +58,6 @@ class ResampleData(object):
 
     def run(self):
         """Runs chosen re-sampling scheme """
-
         # initialise output volume
         self._get_query_points()
         self._init_vols()
@@ -144,15 +149,6 @@ class ResampleData(object):
 
     def _interp_rbf(self):
         """Interpolate using radial basis function"""
-
-        # limit threading to reduce cpu overhead in parallel processes
-        os.environ["MKL_NUM_THREADS"] = "1"
-        os.environ["NUMEXPR_NUM_THREADS"] = "1"
-        os.environ["OMP_NUM_THREADS"] = "1"
-
-        # reload relevant modules for thread changes to take effect
-        importlib.reload(np)
-        importlib.reload(interpolate)
 
         # re-initialise vols
         self._init_vols()
