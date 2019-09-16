65ffcd8ef64ec34807811268f660e2ccb141014f
Laurence Jackson <Laurence.Jackson@kcl.ac.uk>
2019-09-16 16:11:59 +0100

    removed metavar from argparse to prevent parsing errors when input is long

--- sweeprecon/io/ArgParser.py
+++ sweeprecon/io/ArgParser.py
@@ -58,7 +58,6 @@ class ArgParser(object):
 
     def add_input_file(self,
                        option_string=("-i", "--input"),
-                       metavar='',
                        help="path to input file",
                        required=True,
                        type=str
@@ -67,7 +66,6 @@ class ArgParser(object):
 
     def add_target_file(self,
                        option_string=("-tt", "--target"),
-                       metavar='',
                        help="path to target file",
                        required=False,
                        type=str
@@ -76,7 +74,6 @@ class ArgParser(object):
 
     def add_slice_thickness(self,
                             option_string=("-z", "--thickness"),
-                            metavar='',
                             help="thickness of acquired slice [mm]",
                             required=False,
                             default=2.5,
@@ -86,7 +83,6 @@ class ArgParser(object):
 
     def add_crop_fraction(self,
                           option_string=("-cf", "--crop_fraction"),
-                          metavar='',
                           help="fraction of image cropped to segment body area for respiration",
                           required=False,
                           default=0.4,
@@ -96,7 +92,6 @@ class ArgParser(object):
 
     def add_ba_method(self,
                       option_string=("-ba", "--ba_method"),
-                      metavar='',
                       help="method for segmentation body area (gac or cv)",
                       required=False,
                       default='gac',
@@ -106,7 +101,6 @@ class ArgParser(object):
 
     def add_n_resp_states(self,
                           option_string=("-n", "--nstates"),
-                          metavar='',
                           help="number of respiration states",
                           required=False,
                           default=4,
@@ -116,7 +110,6 @@ class ArgParser(object):
 
     def add_interpolator(self,
                          option_string=("-x", "--interpolator"),
-                         metavar='',
                          help="choose interpolater to use [options: 'fast_linear' or 'rbf']",
                          required=False,
                          default='fast_linear'
@@ -125,7 +118,6 @@ class ArgParser(object):
 
     def add_kernel_dims(self,
                         option_string=("-k", "--kernel_dims"),
-                        metavar='',
                         help="Size of interpolation kernel (recommended values 2->4) [default: 2]",
                         required=False,
                         type=int,
@@ -135,7 +127,6 @@ class ArgParser(object):
 
     def add_n_threads(self,
                       option_string=("-t", "--n_threads"),
-                      metavar='',
                       help="number of processor threads to use [default: max(1, N_cpu - 1)]",
                       required=False,
                       type=int,
