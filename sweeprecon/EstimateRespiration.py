73fdc829d941c7b5a4f456798043ab0c0bc987f6
Laurence Jackson <Laurence.Jackson@kcl.ac.uk>
2019-09-18 14:45:00 +0100

    added edge BA method

--- sweeprecon/EstimateRespiration.py
+++ sweeprecon/EstimateRespiration.py
@@ -13,7 +13,8 @@ import sweeprecon.utilities.PlotFigures as PlotFigures
 from multiprocessing import Pool, cpu_count
 from scipy.ndimage import gaussian_filter, morphology, binary_fill_holes
 from scipy.signal import medfilt2d
-from skimage import restoration, measure, segmentation, exposure
+from skimage import restoration, measure, segmentation, exposure, feature, filters
+from skimage import morphology as morphology_skimage
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF, WhiteKernel
 
@@ -176,14 +177,15 @@ class EstimateRespiration(object):
                                                            self._image.img,
                                                            cores=self._n_threads)
 
-            filtered_image = self._process_slices_parallel(self._filter_median,
-                                                           self._image.img,
-                                                           cores=self._n_threads)
+            #filtered_image = self._process_slices_parallel(self._filter_median,
+            #                                               self._image.img,
+            #                                               cores=self._n_threads)
 
             refined_contours = self._process_slices_parallel(self._segment_cv,
                                                              filtered_image,
                                                              self._image_initialised.img,
                                                              cores=self._n_threads)
+
         elif self._args.ba_method == 'gac':
             # filter/pre-process image
             filtered_image = self._process_slices_parallel(self._filter_adaptive_hist_eq,
@@ -194,25 +196,39 @@ class EstimateRespiration(object):
                                                            self._image.img,
                                                            cores=self._n_threads)
 
-            #filtered_image = self._process_slices_parallel(self._filter_median,
-            #                                               filtered_image,
-            #                                               cores=self._n_threads)
+            filtered_image = self._process_slices_parallel(self._filter_median,
+                                                           filtered_image,
+                                                           cores=self._n_threads)
 
             filtered_image = self._process_slices_parallel(self._filter_inv_gauss,
                                                            filtered_image,
                                                            cores=self._n_threads)
-
-            # save filtered image
-            self._image_refined.set_data(filtered_image)
-            self._image_refined.write_nii(self._write_paths.path_filtered_contours())
-
+            # refine contours
             refined_contours = self._process_slices_parallel(self._segment_gac,
                                                              filtered_image,
                                                              self._image_initialised.img,
                                                              cores=self._n_threads)
 
+        elif self._args.ba_method == 'edge':
+
+            filtered_image = self._process_slices_parallel(self._filter_denoise,
+                                                           self._image.img,
+                                                           cores=self._n_threads)
+
+            edge_image = self._process_slices_parallel(self._filter_canny,
+                                                           filtered_image,
+                                                           cores=self._n_threads)
+
+            refined_contours = self._process_slices_parallel(self._segment_flood,
+                                                             edge_image,
+                                                             cores=self._n_threads)
+
         else:
-            print('invalid body area method')
+            raise AssertionError('invalid body area method')
+
+        # save filtered image
+        self._image_refined.set_data(filtered_image)
+        self._image_refined.write_nii(self._write_paths.path_filtered_contours())
 
         # invert mask
         refined_contours = (refined_contours == 0) * 1
@@ -272,10 +288,10 @@ class EstimateRespiration(object):
         return restoration.denoise_tv_bregman(img, weight=weight)
 
     @staticmethod
-    def _filter_inv_gauss(img, alpha=10, sigma=1.5):
+    def _filter_inv_gauss(img, alpha=100, sigma=1.0):
         """
         TV denoising
-        :param imgs: slice to denoise [2D]
+        :param imgs: image to denoise [2D]
         :param weight: TV weight
         :return:
         """
@@ -285,10 +301,31 @@ class EstimateRespiration(object):
     def _filter_adaptive_hist_eq(img):
         """
         adaptive histogram equalisation
-        :param imgs: slice to equalise [2D]
+        :param imgs: image to equalise [2D]
         :return:
         """
         return exposure.equalize_adapthist(img.astype('uint16'), clip_limit=0.02)
+
+    @staticmethod
+    def _filter_canny(img):
+        """
+        canny edge detection
+        :param imgs: image to filter [2D]
+        :return:
+        """
+        return feature.canny(img, sigma=1.5, low_threshold=0.1, high_threshold=0.7, use_quantiles=True)
+
+    @staticmethod
+    def _segment_flood(img):
+        """
+        adaptive histogram equalisation
+        :param imgs: slice to equalise [2D]
+        :return:
+        """
+        img = filters.gaussian(img.astype(int))
+        seg_ant = segmentation.flood(img, (1, 2), selem=morphology_skimage.selem.disk(1))
+        seg_post = segmentation.flood(img, (img.shape[0] - 1, 0), selem=morphology_skimage.selem.disk(1))
+        return seg_ant + seg_post
 
     @staticmethod
     def _segment_cv(img, init_level_set, iterations=100):
@@ -304,11 +341,11 @@ class EstimateRespiration(object):
                                                     init_level_set=init_level_set,
                                                     smoothing=9,
                                                     lambda1=0.5,
-                                                    lambda2=1.5
+                                                    lambda2=3.0
                                                     )
 
     @staticmethod
-    def _segment_gac(img, init_level_set, iterations=100):
+    def _segment_gac(img, init_level_set, iterations=300):
         """
         refines initial segmentation contours using geodesic active contours
         :param imgs: list of 2 images [2D] imgs[0] = slice to segment: imgs[1] = initial level set
@@ -318,7 +355,7 @@ class EstimateRespiration(object):
         return segmentation.morphological_geodesic_active_contour(img,
                                                                   iterations,
                                                                   init_level_set=init_level_set,
-                                                                  smoothing=4,
+                                                                  smoothing=6,
                                                                   balloon=2.5
                                                                   )
 
