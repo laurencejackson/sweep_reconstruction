bf9b289ff3a4595695dbbb9912ed3a16dc846d62
Laurence Jackson <Laurence.Jackson@kcl.ac.uk>
2019-09-24 16:37:48 +0100

    disabled robust statistics

--- sweeprecon/Reconstruction.py
+++ sweeprecon/Reconstruction.py
@@ -157,12 +157,12 @@ class Reconstruction(object):
             "delta": 300,
             "lambda": 0.030,
             # "-cp_spacing": 5.0,
-            "ncc": True,
+            "ncc": False,
             "gaussian_only": False,
             "svr_only": True,
             "no_intensity_matching": True,
             "no_sr": False,
-            "no_robust_statistics": False,
+            "no_robust_statistics": True,
             "reg_log": False,
         }
 
