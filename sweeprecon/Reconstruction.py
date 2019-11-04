d04c98d6c267381cad8795c69e0ea28b6931b723
Laurence Jackson <Laurence.Jackson@kcl.ac.uk>
2019-11-04 15:47:43 +0000

    annoying bug

--- sweeprecon/Reconstruction.py
+++ sweeprecon/Reconstruction.py
@@ -177,15 +177,15 @@ class Reconstruction(object):
         # extract-image-region <input> <output> [options] -patch <i> <j> <k> <nx> [<ny> [<nz>]]
         # normalise image intensity before splitting
 
-        normalise = True
+        normalise = False
         if normalise:
             self._normalise_intensity(image)
 
         # defaults to full image if given dimension is zero
-        self._patchsize_internal = self._args.patchsize
-        
+        _patchsize_internal = copy.deepcopy(self._args.patchsize)
+
         if self._args.patchsize[0] == 0:
-            self._patchsize_internal[0] = image.img.shape[0]
+            _patchsize_internal[0] = image.img.shape[0]
             xlocs = [int(image.img.shape[0]/2)]
         else:
             xlocs = np.arange(int(self._args.patchsize[0]/2),
@@ -193,7 +193,7 @@ class Reconstruction(object):
                               self._args.patchstride[0])
 
         if self._args.patchsize[1] == 0:
-            self._patchsize_internal[1] = image.img.shape[1]
+            _patchsize_internal[1] = image.img.shape[1]
             ylocs =[int(image.img.shape[1] / 2)]
         else:
             ylocs = np.arange(int(self._args.patchsize[1]/2),
@@ -218,7 +218,7 @@ class Reconstruction(object):
             for nx, xi in enumerate(xlocs):
                 for ny, yi in enumerate(ylocs):
                     patch_ind = ny + (nx * xlocs.__len__())
-                    pixel_region = (xi, yi, zlocs, self._patchsize_internal[0], self._patchsize_internal[1], zsize)
+                    pixel_region = (xi, yi, zlocs, _patchsize_internal[0], _patchsize_internal[1], zsize)
                     pixel_region = [str(i) for i in pixel_region]  # convert to string list
                     if target:
                         tstring = ' -Rt1 ' + str(ti) + ' -Rt2 ' + str(ti)
